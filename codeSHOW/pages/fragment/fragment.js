8a8b09b1d7e584d61231c68ae16254dbb6518577
codefoster_cp <SND\codefoster_cp>
2012-09-19 13:29:42 -0700

    Add fragment

---
+++ codeSHOW/pages/fragment/fragment.js
@@ -0,0 +1,23 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/fragment/fragment.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            WinJS.UI.Fragments.render("/pages/fragment/li.html", q("#target"));
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+})();
