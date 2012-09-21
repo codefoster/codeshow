3ea7df29cec873084a2e5988a481161a75e674fe
codefoster_cp <SND\codefoster_cp>
2012-09-21 09:34:23 -0700

    Added some comments to appbar.js

--- codeSHOW/pages/appbar/appbar.js
+++ codeSHOW/pages/appbar/appbar.js
@@ -5,9 +5,17 @@
 
     WinJS.UI.Pages.define("/pages/appbar/appbar.html", {
         ready: function (element, options) {
+            //wire up the onchange event of all radio buttons to the same event
             q(".appbar input[type='radio']").forEach(function (i) { i.onchange = manageAppBar; });
+            
+            //show the appbar (it won't show if it's set to none because it doesn't exist)
             q(".appbar #showBtn").onclick = function () { q("#appbar").winControl.show(); };
+            
+            //hide the appbar
             q(".appbar #hideBtn").onclick = function () { q("#appbar").winControl.hide(); };
+
+            //when the user checks or unchecks the "sticky" checkbox, call Ocho.AppBar.set() to make sure
+            //the appbar exists yet and then set its sticky property
             q(".appbar #sticky").onchange = function (e) {
                 if (!q("#appbar")) Ocho.AppBar.set();
                 q("#appbar").winControl.sticky = e.target.checked;
@@ -18,14 +26,19 @@
     function manageAppBar(e) {
         switch(e.srcElement.id) {
             case "none":
+                //calling Ocho.AppBar.set() without passing anything in effectively clears and disables the appbar
                 Ocho.AppBar.set();
                 break;
             case "basic":
+                //you can create appbar buttons easily... just pass an array into the "buttons" property of the
+                //set method. Notice that the first button is just {} so it gets everything defaulted and does
+                //nothing when you click it
                 Ocho.AppBar.set({
                     buttons: [
+                        {},
                         { label: "Add", icon: "add", click: function () { Windows.UI.Popups.MessageDialog("add").showAsync(); } },
                         { label: "Delete", icon: "delete", click: function () { Windows.UI.Popups.MessageDialog("delete").showAsync(); } },
-                        { label: "Cancel", icon: "cancel", click: function () { Windows.UI.Popups.MessageDialog("cancel").showAsync(); } }
+                        { label: "Cancel", icon: "cancel", click: function () { Windows.UI.Popups.MessageDialog("cancel").showAsync(); } },
                     ]
                 });
                 break;
