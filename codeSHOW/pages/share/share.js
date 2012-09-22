dc127dc2ac70ba75625250bf1e1dca1aa4d53a3f
codefoster_cp <SND\codefoster_cp>
2012-09-21 18:09:27 -0700

    Rename share page from simple_share to just share

--- codeSHOW/pages/share/share.js
+++ codeSHOW/pages/share/share.js
@@ -1,12 +1,12 @@
 ﻿(function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/pages/share_simple/share_simple.html", {
+    WinJS.UI.Pages.define("/pages/share/share.html", {
         ready: function (element, options) {
             WinJS.Binding.processAll(q("body"), options);
             
             Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView().ondatarequested = function(e) {
-                var inputText = q("#inputText").value;
+                var inputText = q(".share #inputText").value;
                 e.request.data.properties.title = (inputText.length > 40 ? inputText.slice(0, 40) + "..." : inputText);
                 e.request.data.setText(inputText);
             };
