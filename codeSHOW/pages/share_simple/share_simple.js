d7c21d9f5325cbb2eacd1c996971bb48cc5a7c12
codefoster_cp <SND\codefoster_cp>
2012-09-23 23:14:01 -0700

    Include simple share demo

---
+++ codeSHOW/pages/share_simple/share_simple.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/share_simple/share_simple.html", {
+        ready: function (element, options) {
+            WinJS.Binding.processAll(q("body"), options);
+            
+            Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView().ondatarequested = function(e) {
+                var inputText = q("#inputText").value;
+                e.request.data.properties.title = (inputText.length > 40 ? inputText.slice(0, 40) + "..." : inputText);
+                e.request.data.setText(inputText);
+            };
+        }
+
+    });
+})();
