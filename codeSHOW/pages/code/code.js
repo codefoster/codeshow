9eaf4bb24aaab13e6af4900300d039f303c476f8
palermo4_cp <SND\palermo4_cp>
2012-09-24 09:55:02 -0700

    Code viewer added

---
+++ codeSHOW/pages/code/code.js
@@ -0,0 +1,31 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/code/code.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            WinJS.xhr({ url: "/js/ocho.js" })
+                    .then(
+                        function (xhr) {
+                            //TODO: add a "working" spinner
+                            var result = xhr.responseText;
+                            q("#codeViewer").innerText = result;
+                        }
+                    )
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+})();
