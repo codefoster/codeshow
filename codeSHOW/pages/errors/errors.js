5957b48c074af2c192e5937f85b14d267a7d5207
palermo4_cp <SND\palermo4_cp>
2012-09-28 14:41:25 -0700

    update to errors

--- codeSHOW/pages/errors/errors.js
+++ codeSHOW/pages/errors/errors.js
@@ -3,55 +3,144 @@
 (function () {
     "use strict";
 
-    var util = Ocho.Utilities;
+    function isTypeOf(o,t) {
+        if (t && t.charAt && t.charAt(0)) {
+            return (Object.prototype.toString.call(o) === "[object " + t + "]");
+        } else {
+            throw new WinJS.ErrorFromName("Args", "'t' required and must be a string.");
+        }
+    }
+
+    function doAsync(msg, limit) {
+        return new WinJS.Promise(
+            // c complete, e error, p progress
+            function (c, e, p) {
+            msg = msg || "no-msg";
+            limit = limit || 3;
+            var seconds = 0;
+            doProgress(msg + ": started"); 
+            var iId = window.setInterval(function () {
+                try {
+                    seconds++;
+                    if (msg === "fail") {
+                        throw new WinJS.ErrorFromName(
+                            "promise", "Muhahaha!");
+                    }
+                    p && p(msg + ": " + seconds.toString());
+                    if (seconds > limit) {
+                        window.clearInterval(iId);
+                        c && c(msg);
+                    }
+                } catch (ex) {
+                    window.clearInterval(iId);
+                    e && e(ex);
+                }
+
+            }, 1000); // repeat every second, stop at limit
+        });
+    }
+
+    function doComplete(msg) {
+        WinJS.log && WinJS.log(msg + ": complete", "demo", "info");
+    }
+    function doProgress(msg) {
+        WinJS.log && WinJS.log(msg, "demo", "info");
+    }
+    function doError(err) {
+        var msg = "undefined error";
+        if (err) {
+            msg = err.detail;
+            if (!(msg && msg.message))
+                msg = err.message || "err type unknown";
+        }
+        WinJS.log && WinJS.log(msg, "demo", "error");
+    }
 
     WinJS.UI.Pages.define("/pages/errors/errors.html", {
 
         stateOfLog: null,
         // This function is called when unhandled errors are captured 
-        error: function (errInfo) {
+        error: function (err) {
             // errInfo parameter has the following properties that are useful
             //      .message
             //      .description
             //      .stack
-
+            WinJS.log && WinJS.log("[page-level] " + err.message, "demo", "error");
         },
 
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+
+            // capture the state of log method for restoring later in unload event
             this.stateOfLog = WinJS.log;
-            // define custom log method
+
+            // initialize custom log method
             WinJS.log = function (msg, tags, type) {
-                // require message only if string
-                if (msg && util.isTypeOf(msg,"String")) {
-                    // assign "info" if not provided
+
+                // msg is required and must be a string
+                if (isTypeOf(msg, "String")) {
+
+                    // default type to "info" if no value provided
                     type = type || "info";
+
+                    // only output msg if tags parameter contains demo
                     if (tags && tags.match && tags.match(/demo/gi)) {
-                        output.innerText = msg + " [" + tags + "]";
+                        // output is the id of a div on the page
+                        output.innerText = msg;
                     }
                     if (type === "error") {
-                        output.setAttribute("class", "error");
+                        output.classList.add("error");
                     } else {
-                        output.removeAttribute("class");
+                        output.classList.remove("error");
                     }
                 }
             };
 
-            boxLog.onclick = function () {
-                WinJS.log && WinJS.log("Log", "demo");
+            boxPromise.onclick = function () {
+                doAsync("promise", 4)
+                    .then(doComplete, doError, doProgress);
             };
-            boxPageError.onclick = function () {
-                WinJS.log && WinJS.log("Page", "demo", "error");
+
+            boxPromiseChainDone.onclick = function () {
+                doAsync("chain[1]", 2)
+                    .then(function (msg) {
+                        doComplete(msg);
+                        return doAsync("chain[2]", 2);
+                    }, null, doProgress)
+                    .then(function (msg) {
+                        doComplete(msg);
+                        return doAsync("fail");
+                    }, null, doProgress)
+                    .then(function (msg) {
+                        doComplete(msg);
+                        return doAsync("chain[4]", 4);
+                    }, null, doProgress)
+                    .done(null, doError);
+            };
+
+            boxPromiseChainJoined.onclick = function () {
+                var promises = [];
+                var i = 0;
+                // each promise has an error handler
+                promises[i++] = doAsync("2seconds", 2)
+                    .then(doComplete, doError);
+                promises[i++] = doAsync("fail", 8)
+                    .then(doComplete, doError);
+                promises[i++] = doAsync("3seconds", 3)
+                    .then(doComplete, doError);
+                // join promises
+                WinJS.Promise.join(promises)
+                    .done(function () {
+                        doProgress("all promises completed");
+                    }, doError);
             };
 
         },
 
-
-
         unload: function () {
-            // TODO: Respond to navigations away from this page.
+
+            // Restore log method to state it was in prior to this page
             WinJS.log = this.stateOfLog;
         },
 
