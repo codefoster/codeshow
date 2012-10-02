2012469afda4a391af6d1f0f4e1681287c072636
palermo4_cp <SND\palermo4_cp>
2012-10-02 04:52:34 -0700

    animations added

---
+++ codeSHOW/pages/animations/animations.js
@@ -0,0 +1,38 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/animations/animations.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+            boxAngry.addEventListener("animationstart", function (arg) {
+                boxAngry.innerText = "Angry";
+            });
+            boxAngry.addEventListener("animationiteration", function (arg) {
+                
+            });
+            boxAngry.addEventListener("animationend", function (arg) {
+                
+            });
+
+    
+    
+            boxAngry.onclick = function () {
+                boxAngry.classList.toggle("animate-get-angry");
+            }
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
+})();
