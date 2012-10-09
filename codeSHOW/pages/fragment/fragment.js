f4197c138d4e3b9615b0438dd179fa878ffb6737
codefoster_cp <SND\codefoster_cp>
2012-10-08 17:17:13 -0700

    Incorporate page rendering into fragment demo

--- codeSHOW/pages/fragment/fragment.js
+++ codeSHOW/pages/fragment/fragment.js
@@ -3,7 +3,14 @@
 
     WinJS.UI.Pages.define("/pages/fragment/fragment.html", {
         ready: function (element, options) {
-            WinJS.UI.Fragments.render("/pages/fragment/li.html", q("#target"));
+            
+            //fragment section
+            WinJS.UI.Fragments.render("/pages/fragment/li.html", q(".fragmentPage #fragment > ul"));
+            
+            WinJS.UI.Fragments.render("http://www.microsoft.com", q(".fragmentPage #fragment2"));
+            
+            //page section
+            WinJS.UI.Pages.render("/pages/flexbox/flexbox.html", q(".fragmentPage #page > div"));
         }
     });
 })();
