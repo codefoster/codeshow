7c47f49c6a1823afcc6ccf88159f62f8c113d05c
codefoster_cp <SND\codefoster_cp>
2012-10-10 22:58:04 -0700

    Code formatting

--- codeSHOW/pages/xhr/xhr.js
+++ codeSHOW/pages/xhr/xhr.js
@@ -5,20 +5,17 @@
         ready: function (element, options) {
             q("#btn1").onclick = function(e) {
                 WinJS.xhr({ url: "http://odata.netflix.com/catalog/Titles?$top=500&$select=Id", headers: {Accept: "application/json"} })
-                    .then(
-                        function (xhr) {
-                            var result = JSON.parse(xhr.response).d
-                                .map(function (r) { return r.Id; })[parseInt(Math.random() * 500)];
-                            return result;
-                        }
-                    ).then(
-                        function(id) {
-                            WinJS.xhr({ url: "http://odata.netflix.com/catalog/Titles('" + id + "')?$select=Name", headers: { Accept: "application/json" } })
-                                .then(function(xhr) {
-                                    q("#results1").innerText = JSON.parse(xhr.response).d.Name;
-                                });
-                        }
-                    );
+                    .then(function (xhr) {
+                        var result = JSON.parse(xhr.response).d
+                            .map(function (r) { return r.Id; })[parseInt(Math.random() * 500)];
+                        return result;
+                    })
+                    .then(function(id) {
+                        WinJS.xhr({ url: "http://odata.netflix.com/catalog/Titles('" + id + "')?$select=Name", headers: { Accept: "application/json" } })
+                            .then(function(xhr) {
+                                q("#results1").innerText = JSON.parse(xhr.response).d.Name;
+                            });
+                    });
             };
 
             q("#btn2").onclick = function(e) {
