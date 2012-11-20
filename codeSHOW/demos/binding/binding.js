2b3dc4de719f31b2428a505aa4ed6216d1b86465
codefoster_cp <SND\codefoster_cp>
2012-11-19 20:33:35 -0800

    Add converter example to the binding demo

--- codeSHOW/demos/binding/binding.js
+++ codeSHOW/demos/binding/binding.js
@@ -7,14 +7,24 @@
             var fred = { firstName: "Fred", lastName: "Johnson", email: "fred@live.com" };
 
             // simpleBinding
-            WinJS.Binding.processAll(q("#person"), fred);
-
-            //TODO: dynamic binding using as
+            WinJS.Binding.processAll(q("#simpleBinding .person", element), fred);
 
             //template            
             var personTemplateCtl = q("#personTemplate").winControl;
             personTemplateCtl.render(fred, q("#renderHere"));
 
+            //converter
+            WinJS.Namespace.define("Demos.binding.Converters", {
+                isAdultConverter: new WinJS.Binding.converter(function(value) {
+                    return (value ? "Adult" : "Child");
+                })
+            });
+            var yakko = { firstName: "Yakko", lastName: "Warner", email: "yakko@live.com", isAdult: false };
+            WinJS.Binding.processAll(q("#converter .person", element), yakko);
+
+            //TODO: dynamic binding using as
+
+            //TODO: add two-way data binding
         }
     });
 })();
