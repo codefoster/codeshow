98f0fd6a6fba09a3205cae835525961240a02272
codefoster_cp <SND\codefoster_cp>
2012-12-06 00:02:10 -0800

    Remove trailing div that is no longer necessary for swiper's Change boxprops so 20 divs in the flexbox are added in JS

--- codeSHOW/demos/boxprops/boxprops.js
+++ codeSHOW/demos/boxprops/boxprops.js
@@ -3,7 +3,7 @@
 
     WinJS.UI.Pages.define("/demos/boxprops/boxprops.html", {
         ready: function (element, options) {
-            
+            for (var i = 0; i < 20; i++) q(".marginbox .flex", element).appendChild(document.createElement("div"));
         }
     });
 })();
