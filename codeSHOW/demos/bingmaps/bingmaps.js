556f54deed49b37213bf092f20388a2cc8269d87
codefoster_cp <SND\codefoster_cp>
2012-12-12 11:17:17 -0800

    Add the pushpin section to the maps demo Move win-type-interactive style rule to default.css so entire project gets pointer (the little hand) when hovering links

--- codeSHOW/demos/bingmaps/bingmaps.js
+++ codeSHOW/demos/bingmaps/bingmaps.js
@@ -3,28 +3,28 @@
 
     WinJS.UI.Pages.define("/demos/bingmaps/bingmaps.html", {
         ready: function (element, options) {
-            //q(".seecode", element).onclick = function(e) {
-            //    WinJS.Navigation.navigate("/pages/demoCode/demoCode.html", options);
-            //};
-            
             Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap, culture: "en - us", homeRegion: "US" });
             function initMap() {
-                var mapOptions = { credentials: "AkkbQwrwIpByEF9ZAnsWJlp3am9qhRzv8Q8GrsOhbNc_TfwFqum6hgiVlOZKRZ6t" };
-
+                var mapOptions = { credentials: "AkkbQwrwIpByEF9ZAnsWJlp3am9qhRzv8Q8GrsOhbNc_TfwFqum6hgiVlOZKRZ6t", width:640, height:480 };
+                var map;
                 //basicMap
-                var basicMap = new Microsoft.Maps.Map(element.querySelector("#basicMap_map"), mapOptions);
-                basicMap.setOptions({ width: 640, height: 480 });
+                map = new Microsoft.Maps.Map(element.querySelector("#basicMap_map"), mapOptions);
                 
                 //moveAround
-                var moveAround = new Microsoft.Maps.Map(element.querySelector("#moveAround_map"), mapOptions);
-                moveAround.setOptions({ width: 640, height: 480 });
+                map = new Microsoft.Maps.Map(element.querySelector("#moveAround_map"), mapOptions);
                 q("#moveAround > button.seattle", element).onclick = function (e) {
                     var seattle = { center: new Microsoft.Maps.Location(47.6215, -122.349329), mapTypeId: Microsoft.Maps.MapTypeId.auto, zoom: 18 };
-                    moveAround.setView(seattle);
+                    map.setView(seattle);
                 };
                 q("#moveAround > button.kauai", element).onclick = function (e) {
                     var kauai = { center: new Microsoft.Maps.Location(22.0833, -159.5), mapTypeId: Microsoft.Maps.MapTypeId.aerial, zoom: 10 };
-                    moveAround.setView(kauai);
+                    map.setView(kauai);
+                };
+
+                //addPushPin
+                map = new Microsoft.Maps.Map(element.querySelector("#addPushPin_map"), mapOptions);
+                q("#addPushPin > button", element).onclick = function (e) {
+                    map.entities.push(new Microsoft.Maps.Pushpin(map.getCenter(), null));
                 };
             }
 
