4072f3872f8932a59674095b54f5474e4b5dd2fe
codefoster_cp <SND\codefoster_cp>
2012-12-26 20:07:18 -0800

    Refactor to page member methods

--- codeSHOW/demos/authentication/authentication.js
+++ codeSHOW/demos/authentication/authentication.js
@@ -6,74 +6,76 @@
 
     WinJS.UI.Pages.define("/demos/authentication/authentication.html", {
         ready: function (element, options) {
-            initSection1();
-            initSection2();
-        }
-    });
+            this.initSection1();
+            this.initSection2();
+        },
+        initSection1: function () {
+            var section1 = q(".authentication #liveSDK");
 
-    function initSection1() {
-        var section1 = q(".authentication #liveSDK");
-        
-        WL.init();
+            WL.init();
 
-        WL.Event.subscribe("auth.login", function () {
-            if (WL.getSession()) log("You are signed in!");
-        });
+            WL.Event.subscribe("auth.login", function () {
+                if (WL.getSession()) log("You are signed in!");
+            });
 
-        WL.Event.subscribe("auth.sessionChange", function () {
-            if (WL.getSession()) log("Your session has changed.");
-        });
+            WL.Event.subscribe("auth.sessionChange", function () {
+                if (WL.getSession()) log("Your session has changed.");
+            });
 
-        q("#signIn", section1).onclick = function (e) {
-            if (WL.getSession()) log("You are already signed in!");
-            else WL.login({ scope: "wl.signin" });
-        };
+            q("#signIn", section1).onclick = function (e) {
+                if (WL.getSession()) log("You are already signed in!");
+                else WL.login({ scope: "wl.signin" });
+            };
 
-        function log(message) {
-            var child = document.createTextNode(message);
-            var logDiv = q("#log", section1);
-            logDiv.appendChild(child);
-            logDiv.appendChild(document.createElement("br"));
-        }
-    }
+            function log(message) {
+                var child = document.createTextNode(message);
+                var logDiv = q("#log", section1);
+                logDiv.appendChild(child);
+                logDiv.appendChild(document.createElement("br"));
+            }
+        },
+        initSection2: function () {
 
-    function initSection2() {
-
-        //microsoft
-        var microsoftSection = q(".authentication #wams #microsoft");
-        q("button", microsoftSection).onclick = function(e) {
-            app.client.login("microsoftaccount")
-                .done(function (results) {
+            //microsoft
+            var microsoftSection = q(".authentication #wams #microsoft");
+            q("button", microsoftSection).onclick = function (e) {
+                app.client.login("microsoftaccount")
+                    .done(function (results) {
                         q("div", microsoftSection).innerText += "User ID: " + results.userId;
-                }, function (error) { /* gulp */ });
-        };
-        
-        ////facebook
-        //var facebookSection = q(".authentication #wams #facebook");
-        //q("button", facebookSection).onclick = function (e) {
-        //    app.client.login("facebook")
-        //        .done(function (results) {
-        //                q("div", facebookSection).innerText += "User ID: " + results.userId;
-        //        }, function (error) { debugger;  });
-        //};
+                    }, function (error) { /* gulp */ });
+            };
 
-        ////twitter
-        //var twitterSection = q(".authentication #wams #twitter");
-        //q("button", twitterSection).onclick = function (e) {
-        //    app.client.login("twitter")
-        //        .done(function (results) {
-        //            q("div", twitterSection).innerText += "User ID: " + results.userId;
-        //        }, function (error) { debugger; });
-        //};
+            ////facebook
+            //var facebookSection = q(".authentication #wams #facebook");
+            //q("button", facebookSection).onclick = function (e) {
+            //    app.client.login("facebook")
+            //        .done(function (results) {
+            //                q("div", facebookSection).innerText += "User ID: " + results.userId;
+            //        }, function (error) { debugger;  });
+            //};
 
-        ////google
-        //var googleSection = q(".authentication #wams #google");
-        //q("button", googleSection).onclick = function (e) {
-        //    app.client.login("google")
-        //        .done(function (results) {
-        //            q("div", googleSection).innerText += "User ID: " + results.userId;
-        //        }, function (error) { debugger; });
-        //};
+            ////twitter
+            //var twitterSection = q(".authentication #wams #twitter");
+            //q("button", twitterSection).onclick = function (e) {
+            //    app.client.login("twitter")
+            //        .done(function (results) {
+            //            q("div", twitterSection).innerText += "User ID: " + results.userId;
+            //        }, function (error) { debugger; });
+            //};
 
-    };
+            ////google
+            //var googleSection = q(".authentication #wams #google");
+            //q("button", googleSection).onclick = function (e) {
+            //    app.client.login("google")
+            //        .done(function (results) {
+            //            q("div", googleSection).innerText += "User ID: " + results.userId;
+            //        }, function (error) { debugger; });
+            //};
+
+        }
+
+
+});
+
+
 })();
