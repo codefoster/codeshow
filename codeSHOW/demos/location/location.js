cce0f3bef331e26b191e3c50c7f3ace5f939cf2e
codefoster_cp <SND\codefoster_cp>
2012-12-26 20:06:38 -0800

    Fix location demo

--- codeSHOW/demos/location/location.js
+++ codeSHOW/demos/location/location.js
@@ -1,20 +1,18 @@
 ﻿(function () {
     "use strict";
 
+    var result = {};
+
     WinJS.UI.Pages.define("/demos/location/location.html", {
         ready: function (element, options) {
-            var loc = new Windows.Devices.Geolocation.Geolocator();
-            var result = { };
-            loc.onstatuschanged = function(e) {
-                debugger;
-            };
-
-            loc.onpositionchanged = function(e) {
-                result.lat = e.position.coordinate.latitude;
-                result.lon = e.position.coordinate.longitude;
-                result.accuracy = format("{0}m", e.position.coordinate.accuracy);
-                WinJS.Binding.processAll(q("section[role=main]"), result);
-            };
+            new Windows.Devices.Geolocation.Geolocator()
+                .onpositionchanged = this.bindPosition;
+        },
+        bindPosition: function(e) {
+            result.lat = e.position.coordinate.latitude;
+            result.lon = e.position.coordinate.longitude;
+            result.accuracy = format("{0}m", e.position.coordinate.accuracy);
+            WinJS.Binding.processAll(q("section[role=main]"), result);
         }
     });
 })();
