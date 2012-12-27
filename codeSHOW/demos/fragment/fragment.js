72188b6dc9c2b20092a36403746f928b70b78dd1
codefoster_cp <SND\codefoster_cp>
2012-12-26 20:05:19 -0800

    Remove one of the sections of fragments demo since it caused an error

--- codeSHOW/demos/fragment/fragment.js
+++ codeSHOW/demos/fragment/fragment.js
@@ -7,7 +7,8 @@
             //fragment section
             WinJS.UI.Fragments.render("/demos/fragment/li.html", q(".fragmentPage #fragment > ul"));
             
-            WinJS.UI.Fragments.render("http://www.microsoft.com", q(".fragmentPage #fragment2"));
+            //unsupported uri for fragment loading
+            //WinJS.UI.Fragments.render("http://www.microsoft.com", q(".fragmentPage #fragment2"));
             
             //page section
             WinJS.UI.Pages.render("/demos/flexbox/flexbox.html", q(".fragmentPage #page > div"));
