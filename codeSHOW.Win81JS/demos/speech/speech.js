9fc627175f253c1aab4303cecf2bc89ae07cdf7f
Jeremy Foster <jeremy.foster@live.com>
2013-08-09 02:21:35 -0700

    add speech demo (incomplete)

---
+++ codeSHOW.Win81JS/demos/speech/speech.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/speech/speech.html", {
+        ready: function (element, options) {
+            //sayit.onclick = function () {
+
+            //    // The object for controlling and playing audio.
+            //    var audio = new Audio();
+
+            //    // The object for controlling the speech-synthesis engine (voice).
+            //    var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+
+            //    // Generate the audio stream from plain text.
+            //    synth.synthesizeTextToStreamAsync(say.value).then(function (markersStream) {
+
+            //        // Convert the stream to a URL Blob.
+            //        var blob = MSApp.createBlobFromRandomAccessStream(markersStream.ContentType, markersStream);
+
+            //        // Send the Blob to the audio object.
+            //        audio.src = URL.createObjectURL(blob, { oneTimeOnly: true });
+            //        audio.play();
+            //    });
+            //};
+        }
+    });
+})();
