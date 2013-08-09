624a872bbfe35a7a28576c53932662cf6c8031d2
Jeremy Foster <jeremy.foster@live.com>
2013-08-09 02:29:12 -0700

    few small changes to the demos page

--- codeSHOW.Win81JS/pages/demos/demos.js
+++ codeSHOW.Win81JS/pages/demos/demos.js
@@ -1,15 +1,33 @@
 ﻿(function () {
     "use strict";
 
+    WinJS.Namespace.define("codeSHOW.Pages.Demos", {
+        pageDataLoaded: false,
+        demosList: new WinJS.Binding.List(),
+        Commands: {
+            demoNavigate: util.markSupportedForProcessing(function (data) {
+                data.detail.itemPromise.then(function (item) {
+                    nav.navigate("/pages/demo/demo.html", item.data);
+                });
+            }),
+        }
+    });
+
     WinJS.UI.Pages.define("/pages/demos/demos.html", {
         ready: function (element, options) {
+            //HERE: this doesn't seem to work here (imperatively) or in the .html (declaratively)
+            demosListView.itemTemplate = document.querySelector("#demoTemplateFoo");
 
-        },
-
-        unload: function () {
-        },
-
-        updateLayout: function (element) {
+            Data.loaded.then(function () {
+                //build demos list
+                Data.demos
+                    .sort(function(a, b) {
+                        return (a.title < b.title ? -1 : 1);
+                    })
+                    .forEach(function(demo) {
+                        codeSHOW.Pages.Demos.demosList.push(demo);
+                    });
+            });
         }
     });
 })();
