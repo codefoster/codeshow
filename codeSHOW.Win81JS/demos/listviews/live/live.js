7eea4a97f479f8daaf4cb10c436cd9357e1a8840
Jeremy Foster <jeremy.foster@live.com>
2013-08-11 01:16:55 -0700

    updated listviews demo to use section folders, move livelist into listviews demo, add .explanation rule to default.css

--- codeSHOW.Win81JS/demos/listviews/live/live.js
+++ codeSHOW.Win81JS/demos/listviews/live/live.js
@@ -1,9 +1,9 @@
 ﻿(function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/demos/livelist/livelist.html", {
+    WinJS.UI.Pages.define("/demos/listviews/live/live.html", {
         init: function(element, options) {
-            WinJS.Namespace.define("codeShow.Demos.listview",{
+            WinJS.Namespace.define("codeShow.Demos.listview.live",{
                 data: new WinJS.Binding.List()
             });
 
@@ -11,13 +11,13 @@
         ready: function (element, options) {
 
             for (var i = 0; i < 10; i++) {
-                codeShow.Demos.listview.data.push(WinJS.Binding.as({ value: 0 }));
+                codeShow.Demos.listview.live.data.push(WinJS.Binding.as({ value: 0 }));
             };
 
             updateNumbers();
 
             function updateNumbers() {
-                codeShow.Demos.listview.data.forEach(function (value, index, array) {
+                codeShow.Demos.listview.live.data.forEach(function (value, index, array) {
                     value.value = Math.floor(Math.random() * 10);
                 });
                 setTimeout(updateNumbers, 1000);
