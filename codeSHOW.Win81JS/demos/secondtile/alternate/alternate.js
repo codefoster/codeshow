9abb26cf37406f4ae62e9df41b1125894417fcd7
Jeremy Foster <jeremy.foster@live.com>
2013-08-19 10:50:23 -0700

    add alternate sectionto secondtile demo

---
+++ codeSHOW.Win81JS/demos/secondtile/alternate/alternate.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/secondtile/alternate/alternate.html", {
+        ready: function (element, options) {
+            element.querySelector("#createSecondaryTile").onclick = function (args) {
+                var tile = new Windows.UI.StartScreen.SecondaryTile(
+                    "SecondaryTile.Demo.secondtile.simple",
+                    "Secondary tile",
+                    "{}",
+                    new Windows.Foundation.Uri("ms-appx:///images/square150.png"),
+                    Windows.UI.StartScreen.TileSize.Square150x150
+                );
+
+                tile.onvisualelementsrequested = function (args) {
+                    args.request.alternateVisualElements[0].square150x150Logo = new Windows.Foundation.Uri("ms-appx:///images/secondary150.png");
+                    args.request.alternateVisualElements[1].square150x150Logo = new Windows.Foundation.Uri("ms-appx:///images/html5.png");
+                };
+                
+                tile.requestCreateAsync().done(function (isCreated) {
+                    if (isCreated) {
+                        element.querySelector("#createSecondaryTileResults").innerText = "Secondary tile has been pinned to your start screen.";
+                    }
+                });
+            };
+        }
+    });
+})();
