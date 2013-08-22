0bb6b6a62a377e91c9c547493d4b9b62d7e52b00
Jeremy Foster <jeremy.foster@live.com>
2013-08-21 22:03:01 -0700

    change app bar sample to simple, add webview/tostring demo

---
+++ codeSHOW.Win81JS/demos/webview/tostring/tostring.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/webview/tostring/tostring.html", {
+        ready: function (element, options) {
+            element.querySelector("x-ms-webview").navigateToString("This is an <b>arbitrary</b> HTML string that I can navigate my WebView to.");
+        }
+    });
+})();
