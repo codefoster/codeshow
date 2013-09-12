aecaa1bf7556f2fd7c0049b30f80e73e2a9ef4af
Jeremy Foster <jeremy.foster@live.com>
2013-09-12 00:21:53 -0700

    upgrade boxprops demo

---
+++ codeSHOW.Win81JS/demos/boxprops/overflow/overflow.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/boxprops/overflow/overflow.html", {
+        ready: function (element, options) {
+        }
+    });
+})();
---
+++ codeSHOW.Win81JS/demos/boxprops/padding/padding.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/boxprops/padding/padding.html", {
+        ready: function (element, options) {
+        }
+    });
+})();
