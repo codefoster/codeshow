e9113548305e18e3ccfba8ca22c5911489a2f13e
Jeremy Foster <jeremy.foster@live.com>
2013-09-12 00:25:48 -0700

    upgrade mediacap demo

--- codeSHOW.Win81JS/demos/mediacap/audio/audio.js
+++ codeSHOW.Win81JS/demos/mediacap/audio/audio.js
@@ -1,34 +1,17 @@
 ﻿(function () {
     "use strict";
 
-    WinJS.UI.Pages.define("/demos/mediacap/mediacap.html", {
+    WinJS.UI.Pages.define("/demos/mediacap/audio/audio.html", {
         ready: function (element, options) {
-            q(".still button").onclick = function () {
-                new Windows.Media.Capture.CameraCaptureUI().captureFileAsync(Windows.Media.Capture.CameraCaptureUIMode.photo)
-                    .done(function (file) {
-                        if (file) q("#capturedImage").src = URL.createObjectURL(file);
-                    });
-            };
-            
-            q(".video button").onclick = function () {
-                var dialog = new Windows.Media.Capture.CameraCaptureUI();
-                dialog.videoSettings.format = Windows.Media.Capture.CameraCaptureUIVideoFormat.mp4;
-                dialog.captureFileAsync(Windows.Media.Capture.CameraCaptureUIMode.video)
-                    .done(
-                        function (file) {
-                            if (file) q("#capturedVideo").src = URL.createObjectURL(file, { oneTimeOnly: true });
-                        }
-                    );
-            };
-
             //TODO: consider https://github.com/katspaugh/wavesurfer.js for visualizing waveform?
             var recording = false;
             var capture;
             var fileName;
-            q(".audio button").onclick = function () {
+            var btn = element.querySelector("section[role=main] button");
+            btn.onclick = function () {
                 if (!recording) {
                     Windows.Devices.Enumeration.DeviceInformation.findAllAsync(Windows.Devices.Enumeration.DeviceClass.audioCapture)
-                        .then(function(devices) {
+                        .then(function (devices) {
                             var captureInitSettings;
                             captureInitSettings = new Windows.Media.Capture.MediaCaptureInitializationSettings();
                             captureInitSettings.audioDeviceId = "";
@@ -41,14 +24,14 @@
 
                                 capture = new Windows.Media.Capture.MediaCapture();
                                 var profile;
-                                capture.initializeAsync(captureInitSettings).then(function(result) {
+                                capture.initializeAsync(captureInitSettings).then(function (result) {
                                     profile = Windows.Media.MediaProperties.MediaEncodingProfile.createWma(Windows.Media.MediaProperties.AudioEncodingQuality.high);
-                                    Windows.Storage.KnownFolders.musicLibrary.createFileAsync("capture.wma", Windows.Storage.CreationCollisionOption.generateUniqueName).then(function(newFile) {
-                                        capture.startRecordToStorageFileAsync(profile, newFile).then(function(result) {
+                                    Windows.Storage.KnownFolders.musicLibrary.createFileAsync("capture.wma", Windows.Storage.CreationCollisionOption.generateUniqueName).then(function (newFile) {
+                                        capture.startRecordToStorageFileAsync(profile, newFile).then(function (result) {
                                             recording = true;
                                             fileName = newFile.name;
-                                            q(".audio button").innerText = "Stop";
-                                            q(".audio button").classList.add("recording");
+                                            btn.innerText = "Stop";
+                                            btn.classList.add("recording");
                                         });
                                     });
                                 });
@@ -58,11 +41,11 @@
                 else {
                     capture.stopRecordAsync();
                     recording = false;
-                    q(".audio button").innerText = "Capture";
-                    q(".audio button").classList.remove("recording");
+                    btn.innerText = "Capture";
+                    btn.classList.remove("recording");
                     Windows.Storage.KnownFolders.musicLibrary.getFileAsync(fileName).then(function (captureFile) {
                         var audioBlobUrl = URL.createObjectURL(captureFile, { oneTimeOnly: true });
-                        q("#capturedAudio").src = audioBlobUrl;
+                        element.querySelector("#capturedAudio").src = audioBlobUrl;
                     });
                 }
             };
