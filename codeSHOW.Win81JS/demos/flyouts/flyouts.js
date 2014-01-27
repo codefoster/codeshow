635928386fd2300306466207fead200f2cc3e6d9
Jeremy Foster <jeremy.foster@live.com>
2014-01-26 19:54:21 -0800

    fix bing maps demo, upgrade flyouts demo

--- codeSHOW.Win81JS/demos/flyouts/flyouts.js
+++ codeSHOW.Win81JS/demos/flyouts/flyouts.js
@@ -3,19 +3,8 @@
 
     WinJS.UI.Pages.define("/demos/flyouts/flyouts.html", {
         ready: function (element, options) {
-            q("#showFlyout",element).onclick = function(e) { q("#flyout1", element).winControl.show(this); };
-            q(".flyouts header .titlearea").onclick = showHeaderMenu;
         }
     });
 
-    function showHeaderMenu() {
-        var title = q(".flyouts header .titlearea");
-        var menu = q(".flyouts #headerMenu").winControl;
-        menu.anchor = title;
-        menu.placement = "bottom";
-        menu.alignment = "left";
-
-        menu.show();
-    }
 
 })();
