e70af467ded70fffa5bd3e60bcdb5dc19590ad9a
Jeremy Foster <jeremy.foster@live.com>
2014-01-26 22:49:16 -0800

    upgrade progressbar demo

---
+++ codeSHOW.Win81JS/demos/progressbar/determinate/determinate.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/progressbar/determinate/determinate.html", {
+        ready: function (element, options) {
+            var p = q(".progressbar .determinate progress");
+
+            q(".progressbar .determinate #advance").onclick = function (e) {
+                p.value += p.max * .1;
+            };
+            q(".progressbar .determinate #reset").onclick = function (e) {
+                p.value = 0;
+            };
+        }
+    });
+})();
