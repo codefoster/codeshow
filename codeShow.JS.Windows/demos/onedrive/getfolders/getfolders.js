e1f2442189cb1f359c9968313ee6daf2742bfce6
iqaanazad <iqaanazad@live.com>
2014-04-19 17:08:18 +0530

    implement ondrive demo

---
+++ codeShow.JS.Windows/demos/onedrive/getfolders/getfolders.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/onedrive/getfolders/getfolders.html", {
+        ready: function (element, options) {
+            WL.init();
+
+            // 'wl.skydrive' gives access to OneDrive but not write permissions
+            // so 'wl.skydrive_update' has been used
+            WL.login({ scope: "wl.skydrive_update" });
+
+            q("#getfoldersbtn").addEventListener("click", getListOfFolders);
+        }
+    });
+})();
+
+function getListOfFolders() {
+
+    if (!WL.getSession()) {
+        q("#folderStatus").innerText = "Not logged in";
+        return;
+    }
+
+    q("#folderStatus").innerText = "Please wait...";
+
+    // path 'me' gets the basic info of the user such as name and profile picture
+    // path 'me/skydrive' returns the properties of onedrive folder
+    // path 'me/skydrive/files' returns all the files+folders+albums in onedrive folder
+    // if you want to fetch any folder other than root folder, you need to have its id which looks like "folder.abcd1234def48shio.ABCD1234DEF48SHIO!987"
+    WL.api({
+        path: "me/skydrive/files?filter=folders,albums",
+        method: "GET"
+    }).then(
+        function (response) {
+            q("#folderList").innerHTML = "";
+
+            // response.data depends on the call supplied to onedrive
+            // in this case it will be an array of all folders
+            response.data.forEach(function (directory) {
+                var l = document.createElement("li");
+                l.innerText = directory.name;
+                q("#folderList").appendChild(l);
+            });
+
+            q("#folderStatus").innerText = "Retrieved " + response.data.length + " folders+albums";
+        },
+        function (response_error) {
+            q("#folderStatus").innerText = "Something went wrong!";
+        }
+    );
+
+}
---
+++ codeShow.JS.Windows/demos/onedrive/pushfile/pushfile.js
@@ -0,0 +1,69 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/onedrive/pushfile/pushfile.html", {
+        ready: function (element, options) {
+            WL.init();
+
+            // 'wl.skydrive' gives access to OneDrive but not write permissions
+            // so 'wl.skydrive_update' has been used
+            WL.login({ scope: "wl.skydrive_update" });
+
+            q("#pushfilebtn").addEventListener("click", pushFile);
+        }
+    });
+})();
+
+function pushFile() {
+
+    if (!WL.getSession()) {
+        q("#pushStatus").innerText = "Not logged in";
+        return;
+    }
+
+    var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+    openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.thumbnail;
+    openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+    openPicker.fileTypeFilter.replaceAll([".png", ".jpg", ".jpeg"]);
+
+    openPicker.pickSingleFileAsync().then(function (file) {
+        if (file) {
+            q("#pushStatus").innerText = "Uploading...";
+
+            WL.backgroundUpload({
+                path: "me/skydrive",
+                file_name: file.name,
+                file_input: file,
+                overwrite: "rename"
+            }).then(
+                    function (response) {
+                        q("#pushStatus").innerText = "Uploaded";
+                    },
+                    function (response_error) {
+                        q("#pushStatus").innerText = "Upload failed";
+                    }
+                );
+        } else {
+            q("#pushStatus").innerText = "No file picked";
+        }
+    });
+
+    WL.api({
+        path: "me/skydrive/files?filter=folders,albums",
+        method: "GET"
+    }).then(
+        function (response) {
+            response.data.forEach(function (directory) {
+                var l = document.createElement("li");
+                l.innerText = directory.name;
+                q("#folderList").appendChild(l);
+            });
+
+            q("#folderStatus").innerText = "Retrieved " + response.data.length + " folders+albums";
+        },
+        function (response_error) {
+            q("#folderStatus").innerText = "Something went wrong!";
+        }
+    );
+
+}
