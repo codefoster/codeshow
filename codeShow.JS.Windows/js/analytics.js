48ed291712bc199c23a6901d63bb7acda44c8c1f
Jeremy Foster <jeremy.foster@live.com>
2014-04-30 12:56:47 -0700

    add missing update statement to analytics.js

--- codeShow.JS.Windows/js/analytics.js
+++ codeShow.JS.Windows/js/analytics.js
@@ -4,7 +4,10 @@
             var analytics = codeshowClient.getTable("analytics");
             analytics.where({ key: key }).read().then(function (rr) {
                 if (rr.length == 0) analytics.insert({ key: key, value: 1 });
-                else rr[0].value++;
+                else {
+                    rr[0].value++;
+                    analytics.update(rr[0]);
+                }
             });
         }
     }
