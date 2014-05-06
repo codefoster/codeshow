acaa26c4a258d75c6f132eab3c25394005f8f9a3
Jeremy Foster <jeremy.foster@live.com>
2014-05-06 12:43:07 -0700

    import CS project, organize projects into folders

---
+++ CodeShow.CS/CodeShow.CS.Shared/ScalingTextDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// BeginCutPaste
+
+namespace CodeShowCsXaml.Shared
+{
+    public sealed partial class ScalingTextDemo : UserControl
+    {
+        public ScalingTextDemo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
+
+// EndCutPaste
---
+++ CodeShow.CS/CodeShow.CS.Shared/TextDemo.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace CodeShowCsXaml.Shared
+{
+    public sealed partial class TextDemo : UserControl
+    {
+        public TextDemo()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
