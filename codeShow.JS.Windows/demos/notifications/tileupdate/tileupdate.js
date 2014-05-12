f26b204d6b7111a3ff0db64b8303f737facb5be0
sebagomez <goomez@outlook.com>
2014-05-12 13:12:37 -0300

    Updated notifications with badges, tiles updates and local notifications via toasts

---
+++ codeShow.JS.Windows/demos/notifications/tileupdate/tileupdate.js
@@ -0,0 +1,36 @@
+﻿(function () {
+	"use strict";
+
+	var element;
+
+	WinJS.UI.Pages.define("/demos/notifications/tileupdate/tileupdate.html", {
+		ready: function (e, options) {
+			element = e;
+			initTileTextSection();
+			initSecondaryTilesSection();
+		}
+	});
+
+	function initTileTextSection() {
+		var updater = Windows.UI.Notifications.TileUpdateManager.createTileUpdaterForApplication();
+
+		q("button.sendText", element).onclick = function (e) {
+			// build and send the tile notification
+
+			//tileContent.textHeadingWrap.text = q("#tileText", element).value;
+			var squareTileContent = NotificationsExtensions.TileContent.TileContentFactory.createTileSquare150x150Text04();
+			squareTileContent.textBodyWrap.text = q("#tileText", element).value;
+			//tileContent.squareContent = squareTileContent;
+			updater.enableNotificationQueue(q("#enableQueueing", element).winControl.checked);
+			updater.update(squareTileContent.createNotification());
+		};
+
+		q("button.clear", element).onclick = function (e) {
+			updater.clear();
+		};
+	}
+
+	function initSecondaryTilesSection() {
+		//TODO: implement creation of secondary tile
+	}
+})();
