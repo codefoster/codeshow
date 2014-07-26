7340a31b9d795682058c57fa91dca96f2683a541
Jeremy Foster <jeremy.foster@live.com>
2014-07-25 23:53:52 -0700

    finish flattening ListView demos, copied search and extra hub sections UI in (not enabled yet)

---
+++ codeShow.JS/codeShow.JS.Shared/demos/asymmetric/asymmetric.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/asymmetric/asymmetric.html", {
+        init: function (element, options) {
+            var images = [
+                { url: "/demos/asymmetric/images/01.jpg", type: "bigsquare" },
+                { url: "/demos/asymmetric/images/02.jpg", type: "square" },
+                { url: "/demos/asymmetric/images/03.jpg", type: "square" },
+                { url: "/demos/asymmetric/images/04.jpg", type: "tall" },
+                { url: "/demos/asymmetric/images/05.jpg", type: "square" },
+                { url: "/demos/asymmetric/images/06.jpg", type: "square" },
+                { url: "/demos/asymmetric/images/07.jpg", type: "square" },
+                { url: "/demos/asymmetric/images/08.jpg", type: "wide" },
+                { url: "/demos/asymmetric/images/09.jpg", type: "square" },
+                { url: "/demos/asymmetric/images/10.jpg", type: "square" }
+            ];
+
+            WinJS.Namespace.define("codeShow.Demos.listviews.asymmetric", {
+                imagesList: new WinJS.Binding.List(images),
+                itemInfoFunction: WinJS.Utilities.markSupportedForProcessing(function (i) {
+                    var size = { width: 120, height: 120 };
+                    if (images[i].type == "bigsquare") { size.width = 250; size.height = 250; }
+                    else if (images[i].type == "wide") size.width = 250;
+                    else if (images[i].type == "tall") size.height = 250;
+                    return size;
+                }),
+                groupInfoFunction: WinJS.Utilities.markSupportedForProcessing(function () {
+                    return { enableCellSpanning: true, cellWidth: 120, cellHeight: 120 };
+                })
+            });
+        }
+    });
+})();
---
+++ codeShow.JS/codeShow.JS.Shared/demos/livelistview/livelistview.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/livelistview/livelistview.html", {
+        init: function(element, options) {
+            WinJS.Namespace.define("codeShow.Demos.livelistview",{
+                data: new WinJS.Binding.List()
+            });
+
+        },
+        ready: function (element, options) {
+            const ITEM_COUNT = 10;
+            const FREQUENCY = 2; //Hz
+
+            for (var i = 0; i < ITEM_COUNT; i++) {
+                codeShow.Demos.livelistview.data.push(WinJS.Binding.as({ value: 0 }));
+            };
+
+            updateNumbers();
+
+            function updateNumbers() {
+                codeShow.Demos.livelistview.data.forEach(function (value, index, array) {
+                    value.value = Math.floor(Math.random() * 10);
+                });
+                setTimeout(updateNumbers, 1/FREQUENCY * 1000);
+            }
+        }
+    });
+})();
