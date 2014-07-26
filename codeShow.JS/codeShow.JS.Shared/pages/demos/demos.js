a6f9316faca258f2b4e6daa8a90a655a7dbe0cf0
Jeremy Foster <jeremy.foster@live.com>
2014-07-26 00:37:47 -0700

    fix demos page not linking to a demo correctly

--- codeShow.JS/codeShow.JS.Shared/pages/demos/demos.js
+++ codeShow.JS/codeShow.JS.Shared/pages/demos/demos.js
@@ -7,7 +7,7 @@
         Commands: {
             demoNavigate: util.markSupportedForProcessing(function (data) {
                 data.detail.itemPromise.then(function (item) {
-                    nav.navigate("/pages/demo/demo.html", { demo: item.data, viewMode: "demo" });
+                    nav.navigate("/pages/demo/demo.html", { demo: item, viewMode: "demo" });
                 });
             }),
         }
