1317c3ea9014bd7c1683067969d030448af692ca
Jeremy Foster <jeremy.foster@live.com>
2014-08-27 07:14:09 -0700

    flatten itemcontainer and rating, add proximity and httpclient

---
+++ codeShow.JS/codeShow.JS.Shared/demos/httpclient/httpclient.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    var httpClient = new Windows.Web.Http.HttpClient();
+
+    WinJS.UI.Pages.define("/demos/httpclient/httpclient.html", {
+        ready: function (element, options) {
+            httpClient.getStringAsync(new Windows.Foundation.Uri("http://www.microsoft.com")).done(function (result) {
+                element.querySelector(".result").innerText = result;
+            });
+        },
+
+        unload: function () {
+            httpClient = null;
+        }
+
+    });
+})();
---
+++ codeShow.JS/codeShow.JS.Shared/demos/proximity/proximity.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/demos/proximity/proximity.html", {
+        ready: function (element, options) {
+            var p = Windows.Networking.Proximity;
+            var findingLabel = element.querySelector(".finding");
+            var proximityDevice = p.ProximityDevice.getDefault();
+            p.PeerFinder.role = p.PeerRole.peer
+            p.PeerFinder.start();
+
+            setTimeout(function () {
+                p.PeerFinder.stop();
+                findingLabel.style.display = "none";
+            }, 5000)
+        }
+    });
+})();
